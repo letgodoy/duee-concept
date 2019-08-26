db4d18065cf69b67bfb7400ef84acb72b8ee3cd0
Let Godoy <leticia@dueebrasil.com.br>
2019-08-26 16:47:03 -0300

    ajustando pra da o build

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,7 +2,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from 'react-helmet'
-import ScrollOut from "scroll-out";
 // import { useStaticQuery, graphql } from "gatsby"
 
 import Logo from '../../images/duee.svg'
@@ -22,33 +21,20 @@ export default class Layout extends React.Component {
     this.cases = []
   }
 
-  componentWillMount() {
-    ScrollOut({
-      onShown: function(el) {
-        // use the web animation API
-        el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-      },
-      onHidden: function(el) {
-        // hide the element initially
-        el.style.opacity = 0;
-      }
-    })
-    }
-
     render() {
       return (
         <>
           <Helmet>
             <script src="https://kit.fontawesome.com/67a84c91e8.js"></script>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" ></link>
-            <script src="https://unpkg.com/scroll-out/dist/scroll-out.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.0.2/dist/simpleParallax.min.js"></script>
+            {/* <script src="https://unpkg.com/scroll-out/dist/scroll-out.min.js"></script> */}
+            {/* <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.0.2/dist/simpleParallax.min.js"></script> */}
           </Helmet>
           {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
           <Navbar urlImg={Logo}/>
 
           <div>
-            <main data-scroll>{this.props.children}</main>
+            <main>{this.props.children}</main>
             {/*<footer>*/}
             {/*  © {new Date().getFullYear()}, Built with*/}
             {/*  {` `}*/}
